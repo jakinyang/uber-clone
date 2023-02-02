923e3e0ac0ea9746dcffcbf4c62d2bf820fd5c45
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-02-01 16:15:23 -0800

    --add google auto complete search feature

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,12 +3,19 @@ import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_API_KEY } from '@env';
+import { useDispatch } from 'react-redux';
+
+// Redux
+import { setOrigin, setDestination } from '../slices/navSlice';
 
 // Components
 import { NavOptions } from '../components/NavOptions';
 
+
 export function HomeScreen() {
   const navigation = useNavigation();
+
+  const dispatch = useDispatch();
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -30,6 +37,31 @@ export function HomeScreen() {
         />
 
         <GooglePlacesAutocomplete 
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          query={{
+            key: GOOGLE_MAPS_API_KEY,
+            language: 'en'
+          }}
+          onPress={(data, details = null) => {
+            console.log("data", data);
+            console.log("details", details);
+            dispatch(setOrigin({
+              location: details.geometry.location,
+              description: data.description
+            }))
+            dispatch(setDestination(null));
+          }}
+          returnKeyType={"search"}
+          fetchDetails={true}
+          minLength={2}
+          enablePoweredByContainer={false}
           placeholder='Where From?'
           nearbyPlacesAPI='GooglePlacesSearch'
           debounce={500}
